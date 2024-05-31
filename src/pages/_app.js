75e922b579874aded2d380ae8d2a6ea537f9b51d
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-05-31 17:15:07 +0900

    DESIGN/폰트설정 변경

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 // pages/_app.js
-import { NextGoogleFonts } from 'next-google-fonts';
+import { GoogleFonts } from 'next-google-fonts';
 import '../app/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
-    <NextGoogleFonts href="https://fonts.googleapis.com/css2?family=Pretendard:wght@400;500;700&display=swap" />
+    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Pretendard:wght@400;500;700&display=swap" />
     return <Component {...pageProps} />;
 }
 
