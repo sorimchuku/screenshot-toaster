c94446a74390defd1c5bb2b899613b79d91dc20e
엄채연 EOM CHAE YEON <sorimchuku@gmail.com>
2025-08-11 17:56:44 +0900

    google tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,15 +35,33 @@ class MyApp extends App {
                     <meta name="description" content={metadata.description} />
                     <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=1' />
                     <meta name="google-site-verification" content="MbuwiZqP4M6LV6EwL5QMHKopoGWLOjL7J3VYNiWe2SI" />
+                                        {/* Google Tag Manager */}
+                    <script>
+                        {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PWK28VFB');
+                        `}
+                    </script>
+                    {/* End Google Tag Manager */}
             </Head>
                 <TemplateProvider>
-                    <div className='flex flex-col h-full w-full'>
+                    <body>
+                                            {/* Google Tag Manager (noscript) */}
+                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWK28VFB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                    {/* End Google Tag Manager (noscript) */}
+                        <div className='flex flex-col h-full w-full'>
                         <TopBar />
                         <div className='flex w-full flex-grow'>
                             <Component {...pageProps} />
                         </div>
                         
                     </div>
+                    </body>
+                    
                     
                 </TemplateProvider>
                 
