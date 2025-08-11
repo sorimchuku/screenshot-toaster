835d525e1de1bf897f79c98c76abffd5bd51186a
엄채연 EOM CHAE YEON <sorimchuku@gmail.com>
2025-08-11 15:19:06 +0900

    Revert "Update _document.js"
    This reverts commit b137afa372dac0c2ec032e97940fe2c288a1bf8a.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,21 +12,10 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link href=' https://cdn.jsdelivr.net/gh/orioncactus/pretendard@1.1.0/dist/web/static/pretendard.css' rel="stylesheet" />
-                    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PWK28VFB');</script>
-<!-- End Google Tag Manager -->
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
-                    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWK28VFB"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
                 </body>
             </Html>
         )
