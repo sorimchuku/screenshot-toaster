1a82fce99280782724ddc9c243b1dfd18dfd4c76
엄채연 EOM CHAE YEON <sorimchuku@gmail.com>
2025-08-11 17:55:13 +0900

    Revert "google tag manager"
    This reverts commit 4017deafd28c525e9cb3a649a8ad31d9d888c2e1.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,24 +11,9 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* Google Tag Manager */}
-                    <script>
-                        {`
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PWK28VFB');
-                        `}
-                    </script>
-                    {/* End Google Tag Manager */}
                     <link href=' https://cdn.jsdelivr.net/gh/orioncactus/pretendard@1.1.0/dist/web/static/pretendard.css' rel="stylesheet" />
                 </Head>
                 <body>
-                    {/* Google Tag Manager (noscript) */}
-                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWK28VFB"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                    {/* End Google Tag Manager (noscript) */}
                     <Main />
                     <NextScript />
                 </body>
